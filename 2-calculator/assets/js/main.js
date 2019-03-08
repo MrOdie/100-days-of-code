fd03781dec4cf30a450a60c437892431d8ffaf30
Sam O'Donnell <sodie5214@gmail.com>
2019-03-07 21:17:35 -0500

    Started day 2

---
+++ 2-calculator/assets/js/main.js
@@ -0,0 +1,22 @@
+let buttons = document.getElementsByClassName('btn');
+let display = document.getElementById('display');
+
+for (let i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
+
+  switch (button.classList.contains("action")) {
+    case true:
+      
+      break;
+    
+    case false:
+      
+      break;
+
+    default:
+
+      break;
+  }
+}
+
+//https://youtu.be/iu-j_T3ki24?t=1746
